6b887d8bdba3cedf427d5272e2ef1bfdd575297b
nlafevers <nathan.lafevers@gmail.com>
2026-09-20 12:05:05 -0400

    feat(app): add launcher icon and smoke unit test

---
+++ app/src/test/java/com/wingspan/app/SmokeTest.kt
@@ -0,0 +1,12 @@
+package com.wingspan.app
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SmokeTest {
+
+    @Test
+    fun additionWorks() {
+        assertEquals(4, 2 + 2)
+    }
+}
